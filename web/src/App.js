970e9f53dbed4c6f9e378e3895153f91ae751585
Emil Trangeled <emilbfh@hotmail.com>
2022-09-20 11:16:15 +0200

    Added routes for usecases

--- web/src/App.js
+++ web/src/App.js
@@ -7,9 +7,37 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/homepage" element={<Homepage/>}/>
+            <Route path="/trials" element={<Trials/>}/>
+            <Route path="/settings" element={<Settings/>}/>
         </Routes>
     </div>
   );
+}
+
+const Settings = () => {
+    return <div>
+        <h1>Settings</h1>
+    </div>
+}
+
+const Trials = () => {
+    return <div>
+        <h1>trials</h1>
+    </div>
+}
+
+const Homepage = () => {
+    return <div>
+        <h1>Homepage</h1>
+    </div>
+}
+
+const Login = () => {
+    return <div>
+        <h1>Login</h1>
+    </div>
 }
 
  const About = () => {
