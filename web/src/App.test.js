52bc23ba1feaaf8be7e57db6bf0be63443dad34a
Fiedler12 <o.lyngholm@gmail.com>
2022-09-20 10:28:52 +0200

    web app added

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -3,8 +3,4 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  let button = screen.getByRole("button");
-  expect(button).toHaveTextContent("Login");
 });
