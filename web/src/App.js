4f1dce9fdeb53e67063493cba03affaaddb405f0
Fiedler12 <o.lyngholm@gmail.com>
2022-09-20 11:13:56 +0200

    web app added

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom";
+import {AppBar, Toolbar, Typography} from "@mui/material";
 
 function App() {
   return (
     <div>
+        <Header/>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
@@ -23,5 +25,17 @@ function App() {
             <h1>Home</h1>
         </div>
  }
+
+ const Header = () => {
+     return (
+         <AppBar position="static">
+             <Toolbar>
+                 <Typography variant="h4">
+                     Navbar
+                 </Typography>
+             </Toolbar>
+         </AppBar>
+     )
+}
 
 export default App;
