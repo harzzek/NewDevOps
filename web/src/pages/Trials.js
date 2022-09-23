e2e5193e162c96a6e4a56ec0b43273e18c501d1d
Fiedler12 <o.lyngholm@gmail.com>
2022-09-23 11:31:51 +0200

    web app added

--- web/src/pages/Trials.js
+++ web/src/pages/Trials.js
@@ -1,7 +1,67 @@
+import {Button, Card, CardActions, Grid, Typography} from "@mui/material";
+
+const trials = [
+    {
+        id: 1,
+        Company: "CompanyName",
+        Trial: "TrialDescription containing different facts and specifications about the trial."
+    },
+    {
+        id: 2,
+        Company: "CompanyName",
+        Trial: "TrialDescription"
+    },
+    {
+        id: 3,
+        Company: "CompanyName",
+        Trial: "TrialDescription"
+    },
+    {
+        id: 4,
+        Company: "CompanyName",
+        Trial: "TrialDescription"
+    },
+    {
+        id: 5,
+        Company: "CompanyName",
+        Trial: "TrialDescription"
+    },
+    {
+        id: 6,
+        Company: "CompanyName",
+        Trial: "TrialDescription"
+    }
+]
+
 function Trials() {
     return(
         <div>
             <h1>Trials</h1>
+            <Grid container
+                  maxWidth="lg"
+                  spacing={4}
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="row">
+                {trials.map(trial => (
+                    <Grid item xs={4}>
+                            <Card sx={6} >
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Trial
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    {trial.Company}
+                                </Typography>
+                                <Typography variant="body2">
+                                    {trial.Trial}
+                                </Typography>
+                                <CardActions>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     )
 
